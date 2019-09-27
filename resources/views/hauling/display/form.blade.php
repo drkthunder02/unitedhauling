51d68e09b9bd67f26d1bb6784c465eb977c6df22
drkthunder02 <mancuso.christopher@gmail.com>
2019-09-26 21:14:57 -0500

    start of form display

---
+++ resources/views/hauling/display/form.blade.php
@@ -0,0 +1,16 @@
+@if(Auth::check())
+@extends('layouts.navbars.loggedin')
+@else
+@extends('layouts.navbars.loggedout')
+@endif
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+
+        </div>
+        <div class="card-body">
+
+        </div>
+    </div>
+</div>
