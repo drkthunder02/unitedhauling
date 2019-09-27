5f9136e73ec103652e218832162dfaf1ac95880b
drkthunder02 <mancuso.christopher@gmail.com>
2019-09-26 20:41:10 -0500

    navbars initial

---
+++ resources/views/layouts/navbars/loggedout.blade.php
@@ -0,0 +1,16 @@
+<div class="container-fluid">
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="/dashboard">United Hauling</a>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdoownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Hauling</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
+                        <a class="dropdown-item" href="/display">Hauling Form</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </div>
