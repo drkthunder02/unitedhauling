0997b0b2ddeb9c4e3849d37bfd72092320249afd
drkthunder02 <mancuso.christopher@gmail.com>
2019-09-27 19:50:21 -0500

    middleware issues

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        //if(!Auth::check()) {
-        //    return route('/');
-        //}
+        if($this->auth->check()) {
+            return route('/');
+        }
     }
 }
