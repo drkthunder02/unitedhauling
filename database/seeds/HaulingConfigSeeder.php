f00dde95aff23cf1498891e2457df7d0d458f438
drkthunder02 <mancuso.christopher@gmail.com>
2019-10-22 21:30:24 -0500

    hauling config seeder

--- database/seeds/HaulingConfigSeeder.php
+++ database/seeds/HaulingConfigSeeder.php
@@ -18,21 +18,21 @@ class HaulingConfigSeeder extends Seeder
             'load_size' => 'small',
             'min_load_size' => 0,
             'max_load_size' => 8000,
-            'price_per_jump' => 600000,
+            'price_per_jump' => 600000.00,
         ]);
 
         HaulingConfig::insert([
             'load_size' => 'medium',
             'min_load_size' => 8000,
             'max_load_size' => 57500,
-            'price_per_jump' => 800000,
+            'price_per_jump' => 800000.00,
         ]);
 
         HaulingConfig::insert([
             'load_size' => 'large',
             'min_load_size' => 57500,
             'max_load_size' => 800000,
-            'price_per_jump' => 1000000,
+            'price_per_jump' => 1000000.00,
         ]);
 
         printf("Finished adding hauling configuration.\r\n");
