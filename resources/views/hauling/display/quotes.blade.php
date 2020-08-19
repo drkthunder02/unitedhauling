1c8301f90464101953726f0c0b657c42af13067d
drkthunder02 <mancuso.christopher@gmail.com>
2020-08-18 23:21:33 -0500

    updated quotes

--- resources/views/hauling/display/quotes.blade.php
+++ resources/views/hauling/display/quotes.blade.php
@@ -12,6 +12,7 @@
                     <th>Min Load Size</th>
                     <th>Max Load Size</th>
                     <th>Price Per Jump</th>
+                    <th>Collateral</th>
                 </thead>
                 <tbody>
                     @foreach($loads as $load)
@@ -20,13 +21,14 @@
                             <td>{{ number_format($load->min_load_size, 2, ".", ",") }}</td>
                             <td>{{ number_format($load->max_load_size, 2, ".", ",") }}</td>
                             <td>{{ number_format($load->price_per_jump, 2, ".", ",") }}</td>
+                            <td>{{ number_format($load->collateral * 100.00, 2, ".", ",")}}%</td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
             <br>
             <div class="container">
-                <h3>Max Collateral should be no more than 2b ISK</h3>
+                <h3>Max Collateral should be no more than 3b ISK</h3>
             </div>
         </div>
     </div>
